3487a7ecd1e1647da7ca85d8c8973a2d130e799d
Jeremy Brown <jeremy.r.brown@gmail.com>
2018-05-16 17:06:48 -0700

    Code to parse arguments from command line
    -10 primes by default
    -able to specify number of primes, n>=1
    -help switch prints command line usage

--- src/main/groovy/com/jeremybrown/primes/PrimeProductTable.groovy
+++ src/main/groovy/com/jeremybrown/primes/PrimeProductTable.groovy
@@ -2,8 +2,64 @@ package com.jeremybrown.primes
 
 class PrimeProductTable
 {
+	static final int N_PRIMES_DEFAULT = 10
+	static final int NO_PRIMES = -1
+	static final HELP_SWITCHES = ["?", "h", "help", "/?", "/h", "/help", "-?", "-h", "-help", "--help"].asImmutable()
+
 	public static void main(String[] args)
 	{
-		println("Hello primes!")
+		int n = parseArgs(args);
+		
+		if(n == -1)
+		{
+			printUsage()
+			System.exit(0)
+		}
+		
+		println("Print $n primes:")
+	}
+	
+	public static int parseArgs(String[] args)
+	{
+		//Parse and return the number of primes to display.
+		//Return -1 to signal printing the usage statement and exiting.
+		
+		//Were any args passed in on the command line?
+		if(args.size() > 0)
+		{
+			//If the first one is an integer...
+			if(args[0].isInteger())
+			{
+				int n = Integer.parseInt(args[0])
+				//Make sure it's not an invalid number of primes
+				if(n <= 0)
+				{
+					System.err.println("Error: <number-of-primes> must be >= 1")
+					return NO_PRIMES
+				}
+				//Otherwise return it. That's how many primes we want to display.
+				return n
+			}
+			//If it's some variant of a command asking for help...
+			else if(HELP_SWITCHES.contains(args[0]))
+			{
+				//...print the usage command and exit
+				return NO_PRIMES
+			}
+			//If the first argument is something else
+			else
+			{
+				System.err.println("Error: Invalid argument " + args[0])
+				return NO_PRIMES
+			}
+		}
+		//Use the default number of primes
+		return N_PRIMES_DEFAULT
+	}
+	
+	static void printUsage()
+	{
+		println("PrimeProductTable <number-of-primes>")
+		println("                  <number-of-primes>: default=10")
 	}
 }
---
+++ src/test/groovy/com/jeremybrown/primes/test/PrimeProductTableTest.groovy
@@ -0,0 +1,39 @@
+package com.jeremybrown.primes.test
+
+import com.jeremybrown.primes.*
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class PrimeProductTableTest
+{
+
+	@Test
+	public void testParseArgs()
+	{
+		//No args. Defaults to N_PRIMES_DEFAULT (10)
+		assert(PrimeProductTable.parseArgs([] as String[]) == PrimeProductTable.N_PRIMES_DEFAULT)
+		
+		//Different options for display help. Returns NO_PRIMES (-1)
+		for(swtch in PrimeProductTable.HELP_SWITCHES)
+		{
+			assert(PrimeProductTable.parseArgs([swtch] as String[]) == PrimeProductTable.NO_PRIMES)
+		}
+		
+		//Not a parseable integer
+		assert(PrimeProductTable.parseArgs(["12b"] as String[]) == PrimeProductTable.NO_PRIMES)
+
+		//Less than 1 prime requested
+		assert(PrimeProductTable.parseArgs(["0"] as String[]) == PrimeProductTable.NO_PRIMES)
+		assert(PrimeProductTable.parseArgs(["-1"] as String[]) == PrimeProductTable.NO_PRIMES)
+		assert(PrimeProductTable.parseArgs(["-2"] as String[]) == PrimeProductTable.NO_PRIMES)
+		
+		//A parseable integer
+		assert(PrimeProductTable.parseArgs(["1"] as String[]) == 1)
+		assert(PrimeProductTable.parseArgs(["36"] as String[]) == 36)
+		assert(PrimeProductTable.parseArgs(["187"] as String[]) == 187)
+		
+	}
+
+}
